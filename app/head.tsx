14ba3ca5654b61a4c3d93c2264ff8d8517229187
Aidan Boland <aidanboland1@icloud.com>
2023-01-14 01:43:12 +0000

    utf-8

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
         <>
             <title>rebar.chat</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta charSet="utf-8" />
             <link rel="icon" href="/favicon.ico" />
         </>
     );
