7bbb822672005de51cc7957960265350945f8bd2
Aidan Boland <aidanboland1@icloud.com>
2023-01-30 12:15:48 +0000

    problem with subtomore messages fixed

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from 'next-auth/react';
 import { useQuery } from '@apollo/client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { getMessageQuery, postMessageQuery, messageSubscriptionQuery } from '../../components/gqlQueries';
 import SignOut from '../../components/signOut';
@@ -12,14 +12,16 @@ import '../../styles/message.scss';
 
 export default function MessagePage() {
     const messageArrayInit: any = [];
-    const [, updateState] = useState();
+    let previousInput: any;
+    const prevInRef = useRef(previousInput);
+    const bottomRef = useRef<HTMLDivElement>(null);
     const [messageArray, setMessageArray] = useState(messageArrayInit);
 
     const { data: session } = useSession();
 
     const { loading, error, data, subscribeToMore } = useQuery(getMessageQuery, {
         onCompleted: (data) => {
-            let tempMessageArray: any = [];
+            let tempMessageArray = [];
             for (let i: number = 0; i < data.getMessages.length; i++) {
                 tempMessageArray.push(data.getMessages[i]);
             }
@@ -31,21 +33,33 @@ export default function MessagePage() {
         subscribeToMore({
             document: messageSubscriptionQuery,
             updateQuery: (prev: any, { subscriptionData }: any) => {
-                if (!subscriptionData.data) return prev;
+                // if (!subscriptionData.data) return prev;
+                console.log('___________________________________________________________________________');
+                console.log('prev no wayayay', prev, prevInRef.current);
+                console.log('new', subscriptionData.data.newMessage);
+                if (subscriptionData.data.newMessage != prevInRef.current) {
+                    prevInRef.current = subscriptionData.data.newMessage;
+                    console.log(subscriptionData.data.newMessage.id, subscriptionData.data.newMessage.content);
+                    let tempMessageArray = [
+                        {
+                            __typename: 'Message',
+                            id: subscriptionData.data.newMessage.id,
+                            content: subscriptionData.data.newMessage.content,
+                            sender: subscriptionData.data.newMessage.sender,
+                        },
+                    ].concat(prev.getMessages);
 
-                let tempMessageArray = [
-                    {
-                        __typename: 'Message',
-                        id: subscriptionData.data.newMessage.id,
-                        content: subscriptionData.data.newMessage.content,
-                        sender: subscriptionData.data.newMessage.sender,
-                    },
-                ].concat(messageArray);
-                subscriptionData = null;
-                setMessageArray(tempMessageArray);
+                    // console.log(tempMessageArray);
+                    subscriptionData = null;
+                    console.log('grr');
+                    return {
+                        getMessages: tempMessageArray,
+                    };
+                }
+                // return;
             },
         });
-    });
+    }, [previousInput, subscribeToMore]);
 
     if (session) {
         if (loading) {
@@ -62,6 +76,12 @@ export default function MessagePage() {
             );
         }
 
+        if (bottomRef && bottomRef.current) {
+            bottomRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        }
+
+        // console.log('previous', prevInRef.current);
+
         return (
             <>
                 <div className='messagePageSignOut'>
@@ -69,7 +89,7 @@ export default function MessagePage() {
                 </div>
 
                 <ul className='messageScroller'>
-                    {messageArray
+                    {data.getMessages
                         .slice(0)
                         .reverse()
                         .map((Message: any) => (
@@ -86,6 +106,10 @@ export default function MessagePage() {
                                 </div>
                             </li>
                         ))}
+
+                    <li>
+                        <div ref={bottomRef} className='scrollTo' />
+                    </li>
                 </ul>
 
                 <TypeBox />
